d7cd08ed95385b9744792d1659b1da46b1a143e1
gTuugii <tuugii83@gmail.com>
2019-04-01 19:07:36 -0500

    Update filterWords.ES6.js

--- Lab1/filterWords.ES6.js
+++ Lab1/filterWords.ES6.js
@@ -1,7 +1,7 @@
 //This *** is ***!
 //Use ES6 feature, not allowed to use for-loop
 String.prototype.filterWords = function(){
-    return this.replace("house", "***").replace("nice", "***");
+    return this.replace(arguments[0], "***").replace(arguments[1], "***");
 };
 
 console.log("This house is nice!".filterWords(['house', 'nice']));
