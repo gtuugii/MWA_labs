22f1f6419376717437cc449ba6b2df17b8438895
gTuugii <tuugii83@gmail.com>
2019-04-03 17:32:18 -0500

    Lab2, Lab3

--- Lab3/resolve4.promise.AsyncAwait.js
+++ Lab3/resolve4.promise.AsyncAwait.js
@@ -1,12 +1,23 @@
 let dns = require('dns');
 let { promisify } = require('util');
-let hostname = "www.mum.edu";
+let hostname = "mum.edu";
 
 let dnsResolve4 = promisify(dns.resolve4);
+
 dnsResolve4(hostname)
     .then(addresses => addresses.forEach(function (ip) {
         console.log('ip: ' + ip);
     }))
     .catch(err => console.log(err));
 
+async function dnsResolveAsync(domain){
+    try {
+        let results = await dnsResolve4(domain);
+        console.log(results);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
 
+dnsResolveAsync(hostname);
